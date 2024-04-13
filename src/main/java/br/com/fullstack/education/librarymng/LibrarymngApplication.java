deb840d4b898eb1d41553af28268f3ebc54f8b5e
Ederson Sparenberger <edsparen@gmail.com>
2024-04-13 17:23:33 -0300

    Initial Commit

---
+++ src/main/java/br/com/fullstack/education/librarymng/LibrarymngApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.education.librarymng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstack/education/librarymng/LibrarymngApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.education.librarymng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
