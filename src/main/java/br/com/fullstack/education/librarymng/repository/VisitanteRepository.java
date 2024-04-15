f53564bb771da196de020b204a8dd0cb46766e53
Eder Sparenberger <edsparen@gmail.com>
2024-04-15 16:36:38 -0300

    fix(visitante): adds bibliotecario MVC files; Adds POST route and method;

---
+++ src/main/java/br/com/fullstack/education/librarymng/repository/VisitanteRepository.java
@@ -0,0 +1,8 @@
+package br.com.fullstack.education.librarymng.repository;
+
+import br.com.fullstack.education.librarymng.entity.VisitanteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitanteRepository extends JpaRepository<VisitanteEntity, Long> {
+
+}
