bbc98aca9daa138625a8ff9f1753bd0f20e47305
Eder Sparenberger <edsparen@gmail.com>
2024-04-16 11:00:26 -0300

    feat(bibliotecarios): adds GET route and method to list all records;

--- src/main/java/br/com/fullstack/education/librarymng/controller/BibliotecarioController.java
+++ src/main/java/br/com/fullstack/education/librarymng/controller/BibliotecarioController.java
@@ -2,16 +2,18 @@ package br.com.fullstack.education.librarymng.controller;
 
 import br.com.fullstack.education.librarymng.controller.dto.BibliotecarioDto;
 import br.com.fullstack.education.librarymng.entity.BibliotecarioEntity;
-import br.com.fullstack.education.librarymng.entity.LivroEntity;
 import br.com.fullstack.education.librarymng.service.BibliotecarioServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,5 +28,10 @@ public class BibliotecarioController {
         BibliotecarioEntity newBibliotecario = bibliotecarioServiceImpl.createBibliotecario(bibliotecario);
         return ResponseEntity.status(HttpStatus.CREATED).body(newBibliotecario);
 
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BibliotecarioEntity>> getAllBibliotecarios(){
+        return ResponseEntity.status(HttpStatus.OK).body(bibliotecarioServiceImpl.getAllBibliotecarios());
     }
 }
