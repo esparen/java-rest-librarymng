93ec0c630a9f2bfb19e2fb73b38053039f2f40ab
Eder Sparenberger <edsparen@gmail.com>
2024-04-16 11:01:01 -0300

    feat(emprestimo): adds GET route and method to list all records;

--- src/main/java/br/com/fullstack/education/librarymng/controller/EmprestimoController.java
+++ src/main/java/br/com/fullstack/education/librarymng/controller/EmprestimoController.java
@@ -12,9 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,5 +47,10 @@ public class EmprestimoController {
         newEmprestimo.setDataDevolucao(emprestimoRequest.getDataDevolucao());
         emprestimoService.createEmprestimo(newEmprestimo);
         return ResponseEntity.status(HttpStatus.CREATED).body(newEmprestimo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmprestimoEntity>> getAllEmprestimos(){
+        return ResponseEntity.status(HttpStatus.OK).body(emprestimoService.getAllEmprestimos());
     }
 }
