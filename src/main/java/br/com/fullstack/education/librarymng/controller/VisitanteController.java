d16becc52ec9684aa48d74d336673a0ece9ff130
Eder Sparenberger <edsparen@gmail.com>
2024-04-16 11:06:11 -0300

    feat(visitantes): adds GET route and method to list all records;

--- src/main/java/br/com/fullstack/education/librarymng/controller/VisitanteController.java
+++ src/main/java/br/com/fullstack/education/librarymng/controller/VisitanteController.java
@@ -8,9 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,5 +26,10 @@ public class VisitanteController {
     public ResponseEntity<VisitanteEntity> createVisitante(@RequestBody VisitanteDto visitante) throws Exception {
         VisitanteEntity newVisitante = visitanteService.createVisitante(visitante);
         return ResponseEntity.status(HttpStatus.CREATED).body(newVisitante);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VisitanteEntity>> getAllVisitantes(){
+        return ResponseEntity.status(HttpStatus.OK).body(visitanteService.getAllVisitantes());
     }
 }
--- src/main/java/br/com/fullstack/education/librarymng/service/VisitanteService.java
+++ src/main/java/br/com/fullstack/education/librarymng/service/VisitanteService.java
@@ -1,10 +1,12 @@
 package br.com.fullstack.education.librarymng.service;
 
-import br.com.fullstack.education.librarymng.controller.dto.BibliotecarioDto;
 import br.com.fullstack.education.librarymng.controller.dto.VisitanteDto;
-import br.com.fullstack.education.librarymng.entity.BibliotecarioEntity;
 import br.com.fullstack.education.librarymng.entity.VisitanteEntity;
+
+import java.util.List;
 
 public interface VisitanteService {
     VisitanteEntity createVisitante(VisitanteDto visitante) throws Exception;
+
+    List<VisitanteEntity> getAllVisitantes();
 }
